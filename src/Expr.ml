46854850304b4a019df542464ef2fd65fa1dbaa9
vikkiorrikki <vikulka58@mail.ru>
2018-02-24 22:31:41 +0300

    home_w1

--- src/Expr.ml
+++ src/Expr.ml
@@ -35,13 +35,13 @@ let update x v s = fun y -> if x = y then v else s y
 (* An example of a non-trivial state: *)                                                   
 let s = update "x" 1 @@ update "y" 2 @@ update "z" 3 @@ update "t" 4 empty
 
-(* Some testing; comment this definition out when submitting the solution. *)
+(* Some testing; comment this definition out when submitting the solution. 
 let _ =
   List.iter
     (fun x ->
        try  Printf.printf "%s=%d\n" x @@ s x
        with Failure s -> Printf.printf "%s\n" s
-    ) ["x"; "a"; "y"; "z"; "t"; "b"]
+    ) ["x"; "a"; "y"; "z"; "t"; "b"] *)
 
 (* Expression evaluator
 
@@ -49,6 +49,37 @@ let _ =
  
    Takes a state and an expression, and returns the value of the expression in 
    the given state.
-*)
-let eval = failwith "Not implemented yet"
+
+let eval = failwith "Not implemented yet"*)
+
+
+let rec calc_bin bin x y = 
+    let int_to_bool x = 
+		if x = 0 then false else true
+    and bool_to_int op_bool x y =
+		if op_bool x y then 1 else 0
+    in
+    match bin with
+    | "+" -> x + y
+    | "-" -> x - y
+    | "*" -> x * y
+    | "/" -> x / y
+    | "%" -> x mod y
+    | "<" -> bool_to_int (<) x y
+    | "<=" -> bool_to_int (<=) x y
+    | ">" -> bool_to_int (>) x y
+    | ">=" -> bool_to_int (>=) x y
+    | "==" -> bool_to_int (==) x y
+    | "!=" -> bool_to_int (<>) x y
+    | "&&" -> bool_to_int (&&) (int_to_bool x) (int_to_bool y)
+    | "!!" -> bool_to_int (||) (int_to_bool x) (int_to_bool y)
+
+let rec eval st expr = 
+    match expr with
+    | Const (value) -> value
+    | Var (value) -> st value
+    | Binop (oper, expr1, expr2) ->
+        let value1 = eval st expr1 
+        and value2 = eval st expr2 
+        in calc_bin oper value1 value2
                     
